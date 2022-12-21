ea5aba8b2d7d2ffbc89f6dea0216eac306c28e94
jantizio <emanuele0246@gmail.com>
2022-12-21 12:15:30 +0100

    aggiunti commenti presi dalle slide nei file h

--- phase1/include/ns.h
+++ phase1/include/ns.h
@@ -1,7 +1,29 @@
-#include "include/pandos_types.h"
+#ifndef NS_H
+#define NS_H
 
+#include <pandos_types.h>
+
+/**
+ * Inizializza tutte le liste dei namespace liberi.
+ * Questo metodo viene invocato una volta sola durante l’inizializzazione della struttura dati.
+ */
 void initNamespaces();
+/**
+ * Ritorna il namespace di tipo type associato al processo p (o NULL).
+ */
 nsd_t *getNamesapce(pcb_t *p, int type);
+/**
+ * Associa al processo p e a tutti i suoi figli il namespace ns.
+ * Ritorna FALSE in caso di errore, TRUE altrimenti.
+ */
 int addNamespace(pcb_t *p, nsd_t *ns);
+/**
+ * Alloca un namespace di tipo type dalla lista corretta.
+ */
 nsd_t *allocNamespace(int type);
+/**
+ * Libera il namespace ns ri-inserendolo nella lista di namespace corretta.
+ */
 void freeNamespace(nsd_t *ns);
+
+#endif // NS_H
