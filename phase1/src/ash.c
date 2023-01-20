37db6f91f345817dd99535cb1638e7185802baee
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2023-01-20 15:53:04 +0100

    proviamo a far partire il test!

--- phase1/src/ash.c
+++ phase1/src/ash.c
@@ -1,7 +1,6 @@
 #include "../include/ash.h"
 #include "hashtable.h"
 #include "list.h"
-#define NULL 0x0
 #include <types.h>
 #include <pandos_const.h>
 #include <pandos_types.h>
@@ -15,13 +14,13 @@ int insertBlocked(int *semAdd, pcb_t *p) {
 
     hash_for_each_possible(semd_h,sem,s_link,*semAdd) { //assumo che avvenga al più un solo giro essendo la chiave univoca (no?)
         list_add_tail(&p->p_list, &sem->s_procq); // non sono affatto sicuro che devo appenderlo per p_list
-        return false;
+        return FALSE;
     }
 
     //SEM non nella ASH -> Bisogna allocarne uno
 
     if(list_empty(&semdFree_h))
-        return true;
+        return TRUE;
 
     //rimuovo il semaforo da semdFree_h
     sem = list_first_entry(&semdFree_h,semd_t,s_freelink);
@@ -33,7 +32,7 @@ int insertBlocked(int *semAdd, pcb_t *p) {
     hash_add(semd_h, &sem->s_link, *semAdd);
 
 
-    return false;
+    return FALSE;
 }
 /**
  * Ritorna il primo PCB dalla coda dei processi bloccati (s_procq) associata al SEMD della ASH con chiave semAdd.
@@ -42,20 +41,26 @@ int insertBlocked(int *semAdd, pcb_t *p) {
  * Se la coda dei processi bloccati per il semaforo diventa vuota,
  * rimuove il descrittore corrispondente dalla ASH e lo inserisce nella coda dei descrittori liberi (semdFree_h).
  */
-pcb_t *removeBlocked(int *semAdd);
+pcb_t *removeBlocked(int *semAdd) {
+    return NULL;
+}
 /**
  * Rimuove il PCB puntato da p dalla coda del semaforo su cui è bloccato (indicato da p->p_semAdd).
  * Se il PCB non compare in tale coda, allora restituisce NULL (condizione di errore). Altrimenti, restituisce p.
  * Se la coda dei processi bloccati per il semaforo diventa vuota,
  * rimuove il descrittore corrispondente dalla ASH e lo inserisce nella coda dei descrittori liberi
  */
-pcb_t *outBlocked(pcb_t *p);
+pcb_t *outBlocked(pcb_t *p) {
+    return NULL;
+}
 /**
  * Restituisce (senza rimuovere) il puntatore al PCB che si trova in testa
  * alla coda dei processi associata al SEMD con chiave semAdd.
  * Ritorna NULL se il SEMD non compare nella ASH oppure se compare ma la sua coda dei processi è vuota.
  */
-pcb_t *headBlocked(int *semAdd);
+pcb_t *headBlocked(int *semAdd) {
+    return NULL;
+}
 /**
  * Inizializza la lista dei semdFree in modo da contenere tutti gli elementi della semdTable.
  * Questo metodo viene invocato una volta sola durante l’inizializzazione della struttura dati.
