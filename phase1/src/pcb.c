59bc27e3407de06b40392e4ef36d4c14ad5649f1
jantizio <emanuele0246@gmail.com>
2023-01-20 11:18:30 +0100

    sistemato pcb.c e pushata configurazione di umps

--- phase1/src/pcb.c
+++ phase1/src/pcb.c
@@ -5,53 +5,59 @@
 static struct list_head pcbFree_h;
 static pcb_t pcbFree_table[MAXPROC];
 
-void initPcbs() // NOTA: non testato, probabilmente non funziona
+void initPcbs()
 {
     INIT_LIST_HEAD(&pcbFree_h);
 
     for (int i = 0; i < MAXPROC; i++)
     {
-        // TODO: forse il bucket dei pcb deve essere un array di puntatori,
-        // e di conseguenza anche la variabile qui sotto
-        struct pcb_t new_pcb;
-        LIST_HEAD(temp);
-        new_pcb.p_list = temp;
-        pcbFree_table[i] = new_pcb;
-
         list_add(&pcbFree_table[i].p_list, &pcbFree_h);
     }
 }
 
-void freePcb(pcb_t *p) // NOTA: non testato, probabilmente non funziona
+void freePcb(pcb_t *p)
 {
+    if (p == NULL) // TODO: controllare se p non è già libero?
+        return;
+
     list_add(&p->p_list, &pcbFree_h);
 }
 
-pcb_t *allocPcb() // NOTA: non testato, probabilmente non funziona
+pcb_t *allocPcb()
 {
     if (list_empty(&pcbFree_h))
         return NULL;
-    else
-    {
-        pcb_PTR toBeAllocated = container_of(pcbFree_h.next, pcb_t, p_list);
-        list_del(pcbFree_h.next);
 
-        // inizializzo i campi
-        INIT_LIST_HEAD(&toBeAllocated->p_list);
+    pcb_PTR toBeAllocated = container_of(pcbFree_h.next, pcb_t, p_list);
+    list_del(pcbFree_h.next);
 
-        toBeAllocated->p_parent = NULL;
-        INIT_LIST_HEAD(&toBeAllocated->p_child);
-        INIT_LIST_HEAD(&toBeAllocated->p_sib);
+    if (toBeAllocated == NULL)
+        return NULL;
 
-        // TODO: capire come inizializzare state_t
-        // toBeAllocated->p_s = {0, 0, 0, 0, NULL, 0, 0};
-        toBeAllocated->p_time = 0;
+    /* init pcb */
+    INIT_LIST_HEAD(&toBeAllocated->p_list);
 
-        toBeAllocated->p_semAdd = NULL;
+    toBeAllocated->p_parent = NULL;
+    INIT_LIST_HEAD(&toBeAllocated->p_child);
+    INIT_LIST_HEAD(&toBeAllocated->p_sib);
 
-        // TODO: capire come allocare i namespace
-        // toBeAllocated->namespaces
+    /* init state_t */
+    toBeAllocated->p_s.entry_hi = 0;
+    toBeAllocated->p_s.cause = 0;
+    toBeAllocated->p_s.status = 0;
+    toBeAllocated->p_s.pc_epc = 0;
+    toBeAllocated->p_s.hi = 0;
+    toBeAllocated->p_s.lo = 0;
+    for (int i = 0; i < STATE_GPR_LEN; i++)
+        toBeAllocated->p_s.gpr[i] = 0;
 
-        return toBeAllocated;
-    }
+    toBeAllocated->p_time = 0;
+
+    toBeAllocated->p_semAdd = NULL;
+
+    // TODO: capire se è giusto allocare i ns così
+    for (int i = 0; i < NS_TYPE_MAX; i++)
+        toBeAllocated->namespaces[i] = NULL;
+
+    return toBeAllocated;
 }
