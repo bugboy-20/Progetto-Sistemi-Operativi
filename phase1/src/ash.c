da190c09132c5a0f1655cd95b0821faf77250149
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2023-02-09 12:21:59 +0100

    La ASH ha passato i test

--- phase1/src/ash.c
+++ phase1/src/ash.c
@@ -11,37 +11,41 @@ static LIST_HEAD(semdFree_h);
 static DEFINE_HASHTABLE(semd_h, 5);
 
 
+static inline semd_t *getSemd(int *semAdd) {
+    semd_t *sem;
+    hash_for_each_possible(semd_h, sem, s_link, hash32_ptr(semAdd)) {
+        if(sem->s_key==semAdd)
+            return sem;
+    }
+    return NULL;
+}
+
 /* Viene inserito il PCB puntato da p nella coda dei processi bloccati associata al SEMD con chiave semAdd.
  * Se il semaforo corrispondente non è presente nella ASH, alloca un nuovo SEMD dalla lista di quelli liberi (semdFree) e lo inserisce nella ASH, settando I campi in maniera opportuna (i.e.key e s_procQ).
  * Se non è possibile allocare un nuovo SEMD perché la lista di quelli liberi è vuota, restituisce TRUE.
  * In tutti gli altri casi, restituisce FALSE.
 */
 int insertBlocked(int *semAdd, pcb_t *p) {
-    semd_t *sem;
+    semd_t *sem=getSemd(semAdd);
+
+    if(sem==NULL) { //SEM non nella ASH -> Bisogna allocarne uno
+
+        if(list_empty(&semdFree_h))
+            return TRUE;
 
 
-    hash_for_each_possible(semd_h,sem,s_link, hash32_ptr(semAdd)) {
-        if(sem->s_key!=semAdd)
-            continue;
-        p->p_semAdd=semAdd;
-        list_add_tail(&p->p_list, &sem->s_procq);
-        return FALSE;
+
+        //rimuovo il semaforo da semdFree_h
+        sem = list_first_entry(&semdFree_h,semd_t,s_freelink);
+        list_del(&sem->s_freelink);
+
+        //set dei campi ed aggiunta
+        sem->s_key=semAdd;
+        INIT_LIST_HEAD(&sem->s_procq);
+        hash_add(semd_h, &sem->s_link, hash32_ptr(semAdd));
     }
 
-    //SEM non nella ASH -> Bisogna allocarne uno
-    if(list_empty(&semdFree_h))
-        return TRUE;
 
-
-
-    //rimuovo il semaforo da semdFree_h
-    sem = list_first_entry(&semdFree_h,semd_t,s_freelink);
-    list_del(&sem->s_freelink);
-
-    //set dei campi ed aggiunta
-    sem->s_key=semAdd;
-    INIT_LIST_HEAD(&sem->s_procq);
-    hash_add(semd_h, &sem->s_link, hash32_ptr(semAdd));
     p->p_semAdd=semAdd;
     list_add_tail(&p->p_list, &sem->s_procq);
 
@@ -58,50 +62,23 @@ int insertBlocked(int *semAdd, pcb_t *p) {
  *      e lo inserisce nella coda dei descrittori liberi (semdFree_h).
 */
 pcb_t *removeBlocked(int *semAdd) {
-    /*
-    semd_t *sem=NULL;
-    pcb_t *pcb=NULL;
-
-    hash_for_each_possible(semd_h, sem, s_link, hash32_ptr(semAdd)) {
-        if(sem->s_key!=semAdd)
-            continue;
-
-        if(list_empty(&sem->s_procq))
-            return NULL;
-        pcb = list_first_entry(&sem->s_procq, pcb_t, p_list);
-        if(pcb == NULL)
-            return NULL;
-
-        list_del(&pcb->p_list);
-        pcb->p_semAdd=NULL;
-
-        if (list_empty(&sem->s_procq)) {
-            hash_del(&sem->s_link);
-            list_add(&sem->s_freelink, &semdFree_h);
-        }
-        return pcb;
-    }
-    return NULL;
-    */
     return outBlocked(headBlocked(semAdd));
 }
 /**
  X Rimuove il PCB puntato da p dalla coda del semaforo su cui è bloccato (indicato da p->p_semAdd).
- ? Se il PCB non compare in tale coda, allora restituisce NULL (condizione di errore). Altrimenti, restituisce p.
+ X Se il PCB non compare in tale coda, allora restituisce NULL (condizione di errore). Altrimenti, restituisce p.
  X Se la coda dei processi bloccati per il semaforo diventa vuota,
  X rimuove il descrittore corrispondente dalla ASH e lo inserisce nella coda dei descrittori liberi
  */
 pcb_t *outBlocked(pcb_t *p) {
 
-    semd_t *sem=NULL;
+    semd_t *sem;
 
-    hash_for_each_possible(semd_h, sem, s_link, hash32_ptr(p->p_semAdd)); //forse anche questo è un metodo leggittimo per interrogare l'ASH
-
-    if(sem==NULL)
+    if(p==NULL || (sem=getSemd(p->p_semAdd))==NULL)
         return NULL;
 
     list_del(&p->p_list);
-    p->p_semAdd=NULL; //immagino...
+    p->p_semAdd=NULL;
 
     if(list_empty(&sem->s_procq)) {
         hlist_del(&sem->s_link);
@@ -116,23 +93,12 @@ pcb_t *outBlocked(pcb_t *p) {
  X Ritorna NULL se il SEMD non compare nella ASH oppure se compare ma la sua coda dei processi è vuota.
  */
 pcb_t *headBlocked(int *semAdd) {
-    semd_t *sem=NULL;
+    semd_t *sem=getSemd(semAdd);
 
-    hash_for_each_possible(semd_h, sem, s_link, hash32_ptr(semAdd)) {
+    if(sem==NULL || list_empty(&sem->s_procq))
+        return NULL;
 
-        if(sem->s_key!=semAdd)
-            continue;
-
-        if(sem==NULL) {
-            addokbuf("sem nullo o vuoto");
-            return NULL;
-        }
-        else if (list_empty(&sem->s_procq)) {
-            addokbuf("sem vuoto");
-            return NULL;
-        }
-        return list_first_entry(&sem->s_procq, pcb_t, p_list);
-    }
+    return list_first_entry(&sem->s_procq, pcb_t, p_list);
 
     return NULL;
 }
