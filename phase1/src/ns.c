7b0f779fd00970155c113d4f1a5ca7521b340aae
auillolo <miky.toscano04@gmail.com>
2023-02-12 12:18:59 +0100

    michele prova

--- phase1/src/ns.c
+++ phase1/src/ns.c
@@ -6,15 +6,43 @@
 #include <pandos_const.h>
 #include <pandos_types.h>
 
+
+/*
+type_nsd[MAX_PROC]: diversi array
+(uno per tipo di namespace! Per
+fase1 basta un singolo namespace:
+PID) di NSD con dimensione
+massima di MAX_PROC.
+• type_nsFree_h: Lista dei NSD di tipo
+type liberi o inutilizzati.
+• type_nsList_h: Lista dei
+namespace di tipo type attivi.
+*/
+static nsd_t PID_nsd[MAX_PROC];
+static LIST_HEAD(PID_nsFree_h);
+static LIST_HEAD(PID_nsList_h);
 /**
  * Inizializza tutte le liste dei namespace liberi.
  * Questo metodo viene invocato una volta sola durante l’inizializzazione della struttura dati.
  */
-void initNamespaces() {}
+void initNamespaces() {
+    for(int i=0; i<MAX_PROC;i++)
+    {
+        PID_nsd[i].n_type = NS_PID;
+        list_add(&PID_nsd[i].n_link, &PID_nsFree_h);
+    }
+}
 /**
  * Ritorna il namespace di tipo type associato al processo p (o NULL).
  */
 nsd_t *getNamespace(pcb_t *p, int type) {
+    for(int i=0;i<NS_TYPE_MAX; i++)
+    {
+        if(p->namespaces[i]->n_type == NS_PID)
+        {
+            return p->namespaces[i];
+        }
+    }    
     return NULL;
 }
 /**
@@ -22,17 +50,25 @@ nsd_t *getNamespace(pcb_t *p, int type) {
  * Ritorna FALSE in caso di errore, TRUE altrimenti.
  */
 int addNamespace(pcb_t *p, nsd_t *ns) {
+    if()
     return FALSE;
 }
 /**
  * Alloca un namespace di tipo type dalla lista corretta.
  */
 nsd_t *allocNamespace(int type) {
+    if(type == NS_PID)
+    {
+        list_move(&PID_nsFree_h, &PID_nsList_h);
+        //return PID_nsList_h;
+    }
     return NULL;
 }
 /**
  * Libera il namespace ns ri-inserendolo nella lista di namespace corretta.
  */
-void freeNamespace(nsd_t *ns) {}
+void freeNamespace(nsd_t *ns) {
+    list_move(&ns, &PID_nsFree_h);
+}
 
 #endif // NS_H
