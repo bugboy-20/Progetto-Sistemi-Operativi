f01bdcddc8a9ea09d60b22654237ca5c68abf03c
auillolo <miky.toscano04@gmail.com>
2023-02-14 11:58:16 +0100

    ns commentati meglio

--- phase1/src/ns.c
+++ phase1/src/ns.c
@@ -6,67 +6,66 @@
 #include <pandos_const.h>
 #include <pandos_types.h>
 
-
-/*
-type_nsd[MAX_PROC]: diversi array
-(uno per tipo di namespace! Per
-fase1 basta un singolo namespace:
-PID) di NSD con dimensione
-massima di MAX_PROC.
-• type_nsFree_h: Lista dei NSD di tipo
-type liberi o inutilizzati.
-• type_nsList_h: Lista dei
-namespace di tipo type attivi.
-*/
-
+/* namespace matrix, an array for every namespace type(NS_TYPE_MAX) with MAXPROC dimension */
 static nsd_t nsd[NS_TYPE_MAX][MAXPROC];
-struct list_head nsFree_h[NS_TYPE_MAX];
-struct list_head nsList_h[NS_TYPE_MAX];
+/*nsFree_h: an array of lists of free namespaces for every type*/
+static struct list_head nsFree_h[NS_TYPE_MAX];
+/*nsFree_h: an array of lists of active namespaces for every type*/
+static struct list_head nsList_h[NS_TYPE_MAX];
 
 void initNamespaces() {
+    //for every namespace type initialize free and active namespaces lists
     for(int j=0; j<NS_TYPE_MAX; j++)
     {
         INIT_LIST_HEAD(&nsFree_h[j]);
         INIT_LIST_HEAD(&nsList_h[j]);
-        for(int i=0; i<MAXPROC;i++)//scorro gli array di tipo j
+        //initialize the j type namespace's array and add the to the free namespaces list
+        for(int i=0; i<MAXPROC;i++)
         {
             nsd[j][i].n_type = j;
-            list_add(&nsd[NS_PID][i].n_link, &nsFree_h[j]);//aggiungo i namespace alla lista di quelli liberi
+            list_add(&nsd[NS_PID][i].n_link, &nsFree_h[j]);
         }
     }
 }
 
 nsd_t *getNamespace(pcb_t *p, int type) {
-    return p->namespaces[type]; //se namespace[type] è null ritorna null, altrimenti ritorna il namespace
+    //returns namespaces[type] because every pcb has only one namespace for every type
+    return p->namespaces[type];
 }
 
 int addNamespace(pcb_t *p, nsd_t *ns) {
-    //si assume un namespace già allocato
+    //assumes already allocated namespace
     if(p !=NULL && ns != NULL){
-        p->namespaces[ns->n_type] = ns;//assegnamento al processo padre
+        //add namespace ns to the father
+        p->namespaces[ns->n_type] = ns;
         pcb_t* i;
-        list_for_each_entry(i, &p->p_child, p_sib) //scorro i figli per assegnare il namespace
+        //cycles the childs to add the namespace ns
+        list_for_each_entry(i, &p->p_child, p_sib) 
         {
             i->namespaces[ns->n_type] = ns;
         }
         return TRUE;
     }
-    return FALSE; // errore in caso di processo o namespace nullo
+    // error if null process or namespace
+    return FALSE; 
 }
 
 nsd_t *allocNamespace(int type) {
     if(!list_empty(&nsFree_h[type]))
     {
+        //move the first free namespace to the active namespaces list of its type
         nsd_t *ns = list_first_entry(&nsFree_h[type], nsd_t, n_link);
-        list_move(&ns->n_link, &nsList_h[type]); //sposta ns nella lista dei namespace liberi del suo tipo
+        list_move(&ns->n_link, &nsList_h[type]);
         return ns;
     }
-    return NULL; //ritorna null se non ci sono namespace liberi
+    //returns null if there are no free namespaces
+    return NULL;
 }
 
 void freeNamespace(nsd_t *ns) {
     if(ns != NULL)
-        list_move(&ns->n_link, &nsFree_h[ns->n_type]); //sposta ns nella lista dei namespace liberi del suo tipo
+        //move ns to the free namespaces list of its type
+        list_move(&ns->n_link, &nsFree_h[ns->n_type]);
 }
 
 #endif // NS_H
