9f877b372b62a397aaa85b73175c2c8fe98ae8a4
Diego R. Ammirabile <diegoroberto.ammirabile@gmail.com>
2023-04-14 12:12:43 +0200

    Hearder e abbozzo di `interrupts`

---
+++ phase2/include/interrupts.h
@@ -0,0 +1,7 @@
+#ifndef INTERRUPS_H
+#define INTERRUPS_H
+
+void interrupt_handler();
+
+
+#endif /* INTERRUPS_H */
--- phase2/src/interrupts.c
+++ phase2/src/interrupts.c
@@ -1,1 +1,28 @@
-// Parte di Simone
+// Parte di Simone, ma c'ha messo le mandi Diego MUAHAHAH
+#include "umps/const.h"
+#include <pandos_types.h>
+#include <pandos_const.h>
+#include <umps/types.h>
+/*
+ * Interrupt Line   |   Device Class
+ *  -----------------------------------------------
+ *              0   |   Inter-processor interrupts
+ *              1   |   Processor Local Timer
+ *              2   |   Bus (Interval Timer)
+ *              3   |   Disk Devices
+ *              4   |   Tape Devices
+ *              5   |   Network (Ethernet) Devices
+ *              6   |   Printer Devices
+ *              7   |   Terminal Devices
+ */
+void interrupt_handler() {
+    //state_t *state = (memaddr) BIOSDATAPAGE;
+    unsigned int cause = ((state_t*) BIOSDATAPAGE)->cause;
+
+
+    if (cause & TIMERINTERRUPT);
+    if (cause & DISKINTERRUPT);
+    if (cause & FLASHINTERRUPT);
+    if (cause & PRINTINTERRUPT);
+    if (cause & TERMINTERRUPT);
+    }
