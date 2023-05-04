8733b185bb492c269c633410d83ecfe222c38cd5
konova8 <canovasimo@gmail.com>
2023-05-04 10:55:19 +0200

    Possibile implementazione per interrupt dei terminali

--- phase2/src/interrupts.c
+++ phase2/src/interrupts.c
@@ -12,64 +12,95 @@
 #include <scheduler.h>
 #include <syscall.h>
 
-#define IDBM 0x10000040             // Interrupt Devices BitMap
-#define TERMINALBM  IDBM + 0x10     // Interrupt Line 7 Interrupting Devices Bit Map
-#define PRINTERBM   IDBM + 0x0C     // Interrupt Line 6 Interrupting Devices Bit Map
-#define NETWORKBM   IDBM + 0x08     // Interrupt Line 5 Interrupting Devices Bit Map
-#define FLASHBM     IDBM + 0x04     // Interrupt Line 4 Interrupting Devices Bit Map
-#define DISKBM      IDBM + 0x00     // Interrupt Line 3 Interrupting Devices Bit Map
+#define IDBM 0x10000040        // Interrupt Devices BitMap
+#define TERMINALBM IDBM + 0x10 // Interrupt Line 7 Interrupting Devices Bit Map
+#define PRINTERBM IDBM + 0x0C  // Interrupt Line 6 Interrupting Devices Bit Map
+#define NETWORKBM IDBM + 0x08  // Interrupt Line 5 Interrupting Devices Bit Map
+#define FLASHBM IDBM + 0x04    // Interrupt Line 4 Interrupting Devices Bit Map
+#define DISKBM IDBM + 0x00     // Interrupt Line 3 Interrupting Devices Bit Map
+
+#define RT_STATUS 0xFF
 
 // calculate the address of the device's device register
 /*inline memaddr devAddrBase(int IntlineNo, int DevNo) {
     return 0x10000054 + ((IntlineNo - 3) * 0x80) + (DevNo * 0x10);
 }*/
 
-#define devAddrBase(IntlineNo, DevNo) (memaddr) (0x10000054 + ((IntlineNo - 3) * 0x80) + (DevNo * 0x10))
+#define devAddrBase(IntlineNo, DevNo) (memaddr)(0x10000054 + ((IntlineNo - 3) * 0x80) + (DevNo * 0x10))
 
-static inline int getDevNo(const int bitMap) {
-    for(char i = 0; i < 8; i++) 
-        if(1 << i & bitMap)
+static inline int getDevNo(const int bitMap)
+{
+    for (char i = 0; i < 8; i++)
+        if (1 << i & bitMap)
             return i;
-    
-    return -1; //Error
+
+    return -1; // Error
 }
 
 // TODO workaround, bisogna capire perchè non lo trova da solo il compilatore
 // extern void verhogen(int *semAddr);
 
-void notTimerInterrupt(int IntlineNo, int DevNo) {
-    unsigned int status;
-    bool read;
+void dtpInterruptHandler(int IntlineNo, int DevNo)
+{
     // Calculate the address for this device's register
-    dtpreg_t *dev = (dtpreg_t*) devAddrBase(IntlineNo, DevNo);
-    // TODO Caso dei terminali
-    if (IntlineNo == TERMINT) {
-        termreg_t* ter = (termreg_t*) dev;
-        read = ter->transm_status == READY;
-        if(!read)
-        {
-            status = ter->transm_status;
-            ter->transm_command = ACK;
-        }
-        else
-        {
-            status = ter->recv_status;
-            ter->recv_command = ACK;
-        }
-    }
-    else {
-        // Save off the status code from the device’s device register
-        status = dev->status;
-        // Acknowledge the outstanding interrupt. This is accomplished by writing the acknowledge command code in the interrupting device’s device register
-        dev->command = ACK;
-    }
+    dtpreg_t *dev = (dtpreg_t *)devAddrBase(IntlineNo, DevNo);
+    // Save off the status code from the device’s device register
+    unsigned int status = dev->status;
+    // Acknowledge the outstanding interrupt. This is accomplished by writing the acknowledge command code in the interrupting device’s device register
+    dev->command = ACK;
     // Perform a V operation on the Nucleus maintained semaphore associated with this (sub)device. This operation should unblock the process (pcb) which initiated this I/O operation and then requested to wait for its completion via a SYS5 operation.
-    // TODO gestire terminali in scrittura
-    int* devSem = !read ? (int*) dev_sem_addr(IntlineNo, DevNo) : (int*) dev_sem_addr(IntlineNo, DevNo+8);
-    pcb_t* proc = headBlocked(devSem);
+    int *devSem = (int *)dev_sem_addr(IntlineNo, DevNo);
+    pcb_t *proc = headBlocked(devSem);
     verhogen(devSem);
     // If this process is present
-    if (proc != NULL) {
+    if (proc != NULL)
+    {
+        // Place the stored off status code in the newly unblocked pcb’s v0 register.
+        proc->p_s.reg_v0 = status;
+        // Insert the newly unblocked pcb on the Ready Queue, transitioning this process from the “blocked” state to the “ready” state.
+        // Already done by the Verhogen
+    }
+    else
+    {
+        // Verhogen decrement `soft_block_count` only if process != NULL, this needs to be decremented always 'cause of the DOIO syscall
+        soft_block_count -= 1;
+    }
+    if (current_proc != NULL)
+        LDST(EXCEPTION_STATE); // Return control to the Current Process: Perform a LDST on the saved exception state
+    else
+        scheduler(); // Call the scheduler;
+}
+
+void termInterruptHandler(int IntlineNo, int DevNo)
+{
+    unsigned int status;
+    int *devSem;
+    // Calculate the address for this device's register
+    termreg_t *term = &(((devreg_t *)devAddrBase(IntlineNo, DevNo))->term);
+    if ((term->transm_status & RT_STATUS) > READY)
+    {
+        // Save off the status code from the device’s device register
+        status = term->transm_status;
+        // Acknowledge the outstanding interrupt. This is accomplished by writing the acknowledge command code in the interrupting device’s device register
+        term->transm_command = ACK;
+        // Perform a V operation on the Nucleus maintained semaphore associated with this (sub)device. This operation should unblock the process (pcb) which initiated this I/O operation and then requested to wait for its completion via a SYS5 operation.
+        devSem = (int *)dev_sem_addr(IntlineNo, DevNo);
+    }
+    else if ((term->recv_status & RT_STATUS) > READY)
+    {
+        // Save off the status code from the device’s device register
+        status = term->recv_status;
+        // Acknowledge the outstanding interrupt. This is accomplished by writing the acknowledge command code in the interrupting device’s device register
+        term->recv_command = ACK;
+        // Perform a V operation on the Nucleus maintained semaphore associated with this (sub)device. This operation should unblock the process (pcb) which initiated this I/O operation and then requested to wait for its completion via a SYS5 operation.
+        devSem = (int *)dev_sem_addr((IntlineNo + 1), DevNo);
+    }
+
+    pcb_t *proc = headBlocked(devSem);
+    verhogen(devSem);
+    // If this process is present
+    if (proc != NULL)
+    {
         // Place the stored off status code in the newly unblocked pcb’s v0 register.
         proc->p_s.reg_v0 = status;
         // Insert the newly unblocked pcb on the Ready Queue, transitioning this process from the “blocked” state to the “ready” state.
@@ -78,7 +109,6 @@ void notTimerInterrupt(int IntlineNo, int DevNo) {
     // Return control to the Current Process: Perform a LDST on the saved exception state
     state_t *state = EXCEPTION_STATE;
     LDST(state);
-
 }
 
 /*
@@ -93,15 +123,17 @@ void notTimerInterrupt(int IntlineNo, int DevNo) {
  *              6   |   Printer Devices
  *              7   |   Terminal Devices
  */
-void interrupt_handler() {
+void interrupt_handler()
+{
     unsigned int cause = EXCEPTION_STATE->cause; // Custom system for getting cause
 
-    // ignoring interrupt line 0 
+    // ignoring interrupt line 0
 
     // PLT exception, line 1
-    if (cause & LOCALTIMERINT) {
+    if (cause & LOCALTIMERINT)
+    {
         // Acknowledge the PLT interrupt, and load the timer again
-        setTIMER(TIMESLICE * (*((cpu_t*) TIMESCALEADDR)));
+        setTIMER(TIMESLICE * (*((cpu_t *)TIMESCALEADDR)));
         // Copy the processor state in the current process status
         current_proc->p_s = *EXCEPTION_STATE;
         // Move the process from running to ready
@@ -110,7 +142,8 @@ void interrupt_handler() {
         scheduler();
     }
     // Interval Timer exception, line 2
-    else if (cause & TIMERINTERRUPT) {
+    else if (cause & TIMERINTERRUPT)
+    {
         // Load the Interval Timer with 100ms
         LDIT(PSECOND);
         // Unblock ALL pcbs blocked on the pseudo-clock semaphore
@@ -130,15 +163,14 @@ void interrupt_handler() {
             LDST(EXCEPTION_STATE);
         else
             scheduler();
-
     }
     // Line 3-7, these ar the devices
     else if (cause & DISKINTERRUPT)
-        notTimerInterrupt(DISKINT, getDevNo(DISKBM));
+        dtpInterruptHandler(DISKINT, getDevNo(DISKBM));
     else if (cause & FLASHINTERRUPT)
-        notTimerInterrupt(FLASHINT, getDevNo(FLASHBM));
+        dtpInterruptHandler(FLASHINT, getDevNo(FLASHBM));
     else if (cause & PRINTINTERRUPT)
-        notTimerInterrupt(PRNTINT, getDevNo(PRINTERBM));
+        dtpInterruptHandler(PRNTINT, getDevNo(PRINTERBM));
     else if (cause & TERMINTERRUPT)
-        notTimerInterrupt(TERMINT, getDevNo(TERMINALBM));
+        termInterruptHandler(TERMINT, getDevNo(TERMINALBM));
 }
