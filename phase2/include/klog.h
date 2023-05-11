aa5318d17589643c6745a4dc3c01c1ea0a4e8d27
Diego R. Ammirabile <diegoroberto.ammirabile@gmail.com>
2023-05-11 13:14:57 +0200

    Macro klog sistemare quello fatto da @konova8

--- phase2/include/klog.h
+++ phase2/include/klog.h
@@ -5,4 +5,6 @@ void klog_print_hex(unsigned int num);
 
 void klog_print(char *str);
 
+#define KLOG_PRETTI_PRINT(text, var) klog_print(text); klog_print_hex(var); klog_print("\n")
+
 #endif // KLOG_H
