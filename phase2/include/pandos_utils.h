a4e28347eb40f5ff08ce3b4cf0e1c2ee8add855f
jantizio <emanuele0246@gmail.com>
2023-06-07 08:08:10 +0200

    definizione macro per il tempo

--- phase2/include/pandos_utils.h
+++ phase2/include/pandos_utils.h
@@ -7,7 +7,7 @@
 
 #define NUMSEM (DEVICECNT + 8)
 
-extern passupvector_t* pass_up_vector;
+extern passupvector_t *pass_up_vector;
 extern int process_count;
 extern int soft_block_count;
 extern pcb_PTR current_proc;
@@ -17,26 +17,25 @@ extern int pseudoclock_semaphore;
 extern cpu_t start_time;
 extern int *value_bak;
 
-
-
-
 // Returns the address of the semaphore related to a specific device
 // type : type of device use DISKINT, FLASHINT, ecc... (defined in umps/const.h)
-#define dev_sem_addr(type, n) ((memaddr) &sem_table[(type - 3) * DEVPERINT + n])
+#define dev_sem_addr(type, n) ((memaddr)&sem_table[(type - 3) * DEVPERINT + n])
 
-#define  EXCEPTION_STATE ((state_t*)BIOSDATAPAGE)
+#define EXCEPTION_STATE ((state_t *)BIOSDATAPAGE)
 
 #define POG print("\tPOG\n")
 
 typedef int size_t;
 // copia porzione di memoria
 // possibile rischio di buffer overflow
-void *memcpy(void *dest, const void * src, size_t n);
-
+void *memcpy(void *dest, const void *src, size_t n);
 
 // Definition of binary semaphore operation
 bool P(int *);
 bool V(int *);
 
+#define time_now(var) \
+    cpu_t var;        \
+    STCK(var);
 
 #endif /* UTILS_H */
